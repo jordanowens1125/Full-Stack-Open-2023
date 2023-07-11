70369299fc68494fce2987ed79c2aab5cd26242b
Jordan Owens <jordanowens1125@gmail.com>
2023-07-11 11:15:29 -0500

    Worked on part3

--- part3/notes app/backend/index.js
+++ part3/notes app/backend/index.js
@@ -43,8 +43,54 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+// get notes
 app.get("/api/notes", (request, response) => {
   response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// new notes
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+// del notes
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
 
 app.listen(PORT, () => {
