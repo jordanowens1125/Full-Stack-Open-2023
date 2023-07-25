224b47daf49e62c11d433661834b0772a9f026a2
Jordan Owens <jordanowens1125@gmail.com>
2023-07-25 10:25:51 -0500

    -4.2

---
+++ part4/notes/backend/tests/note_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("all notes are returned", async () => {
+  const response = await api.get("/api/notes");
+
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test("a specific note is within the returned notes", async () => {
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only JavaScript");
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
